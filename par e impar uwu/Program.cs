bfd3978dbf73fd702412f58da589876a4c564621
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-07 02:30:47 -0600

    Agregar archivos de proyecto.

---
+++ par e impar uwu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace par_e_impar_uwu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            numerospi uwu = new numerospi();
+
+            uwu.numeros();
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ par e impar uwu/numerospi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace par_e_impar_uwu
+{
+    class numerospi
+    {
+        int numero1;
+        int respuesta;
+
+
+        public void numeros()
+        {
+
+            Console.WriteLine("Numeros par e impar. ");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Escribe un numero: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            respuesta = numero1 % 2;
+
+            if (respuesta == 0)
+            {
+                Console.WriteLine("El numero {0} es par", numero1);
+
+
+            }
+
+            else
+
+                Console.WriteLine("El numero {0} es impar", numero1);
+
+
+
+        }
+
+    }
+}
